0fe82386fda4120b8173ae19983cb02d8e1e5315
andresterrazas <122042169@upq.edu.mx>
2025-05-25 18:29:21 -0600

    Ejercicio B práctica 3 terminado.

--- Practica3/main.js
+++ Practica3/main.js
@@ -3,3 +3,22 @@ import { restar } from "./utils.js";
 //Pruebas para la función restar
 console.log(restar(5, 3));
 console.log(restar(10, 5));
+
+//Ejercicio B
+
+function verificarUsuario(usuario) {
+    return new Promise((resolve, reject) => {
+        if (usuario === "admin") {
+            resolve("Acceso concedido");
+        } else {
+            reject("Acceso denegado");
+        }
+    });
+}
+
+verificarUsuario("admin")
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+verificarUsuario("user")
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
