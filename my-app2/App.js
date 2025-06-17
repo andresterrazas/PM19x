037c43e76291e32bce7bac85735e34d09d285b85
andresterrazas <122042169@upq.edu.mx>
2025-06-16 19:43:23 -0600

    Práctica 5 y práctica 6 terminadas, no pude hacer la práctica 5 en tiempo y forma, por eso es un commit para ambas.

--- my-app2/App.js
+++ my-app2/App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-web";
+import React, { useState } from "react";
+
+const Texto = ({ style }) => {
+  const [contenido, setContenido] = useState("Hola Mundo");
+  const actualizarTexto = () => {
+    setContenido("State Modificado");
+  };
+  return (
+    <Text style={[styles.text, style]} onPress={actualizarTexto}>
+      {" "}
+      {contenido}{" "}
+    </Text>
+  );
+};
 
 export default function App() {
+  const [contenido, setContenido] = useState("Aque no me tocas");
+  const actualizarBoton = () => {
+    setContenido("Aplastado");
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Texto style={styles.rojo}></Texto>
+      <Texto style={styles.verde}></Texto>
+      <Texto style={styles.amarillo}></Texto>
+      <Button onPress={actualizarBoton} title={contenido}></Button>
     </View>
   );
 }
@@ -13,8 +35,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+    justifyContent: "space-evenly",
+    flexDirection: "column",
   },
+  text: {
+    color: "white",
+    fontSize: 28,
+    width: 100,
+    height: 100,
+  },
+  rojo: { backgroundColor: "red" },
+  amarillo: { backgroundColor: "yellow" },
+  verde: { backgroundColor: "green" },
 });
