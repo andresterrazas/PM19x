e43f588ab9703d924d5d6a3a1803748fdc1c047e
andresterrazas <122042169@upq.edu.mx>
2025-06-25 22:25:44 -0600

    práctica 10 terminada.

--- my-app2/index.js
+++ my-app2/index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './practica09';
+import App from './practica10';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
