407e592393d33a117448dc345a00b05402c24080
andresterrazas <122042169@upq.edu.mx>
2025-07-02 19:55:43 -0600

    práctica 12 scrollview terminada.

--- my-app2/index.js
+++ my-app2/index.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './practica11';
+import App from './scrollview';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
